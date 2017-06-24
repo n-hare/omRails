e2888b8932e8ce8bf2b282be3c42af8f35d10d6d
Nathan Hare <n.hare@hotmail.com>
2017-06-24 15:33:46 -0400

    Adds users with Devise

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
 //= require rails-ujs
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
