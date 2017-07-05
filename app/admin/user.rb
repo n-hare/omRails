324c49782aab740e51a4926c0ff749dbf4d7f371
Nathan Hare <n.hare@hotmail.com>
2017-07-05 01:00:17 -0400

    Adds username for users

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -11,11 +11,12 @@ ActiveAdmin.register User do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-    permit_params :email, :admin
+    permit_params :email, :admin, :username
     form do |f|
         f.semantic_errors
         f.inputs do
             f.input :email
+            f.input :username
             f.input :admin
         end
         f.actions
@@ -24,6 +25,7 @@ ActiveAdmin.register User do
     index do
         id_column
         column :email
+        column :username
         column :admin
         actions
     end  
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
             :rememberable, :trackable, :validatable
     has_many :questions
     has_many :answers
+
+    validates :username, presence: true, uniqueness: true
 end
